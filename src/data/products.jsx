2983e9fa6db880f226fc3ecc5f9f842b9030a6ac
Sinchana7026 <sinchanadevadiga221@gmail.com>
2024-11-17 22:37:48 +0530

    Add files via upload

---
+++ src/data/products.jsx
@@ -0,0 +1,42 @@
+// src/data/products.js
+const products = [
+    {
+      id: 1,
+      name: 'Chocolate Cake',
+      price: 20,
+      image: '/chocalate.jpg',
+      description: 'Delicious chocolate cake with rich flavor.'
+    },
+    {
+      id: 2,
+      name: 'Vanilla Cake',
+      price: 18,
+      image: 'venila.jpg',
+      description: 'Light and fluffy vanilla cake.'
+    },
+    {
+        id: 2,
+        name: 'Red Velvet Cake',
+        price: 18,
+        image: '/red.jpg',
+        description: 'fluffy red velvet cake.'
+      },
+      {
+        id: 2,
+        name: 'butter Scotch Cake',
+        price: 18,
+        image: '/butter.jpg',
+        description: 'Light and fluffy vanilla cake.'
+      },
+      {
+        id: 2,
+        name: 'Black Current Cake',
+        price: 18,
+        image: '/black.jpg',
+        description: 'Black Current cake.'
+      },
+    // Add more products as needed
+  ];
+  
+  export default products;
+  
